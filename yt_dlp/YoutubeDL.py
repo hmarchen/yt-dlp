8e4be201e1f2dda1f1e1943ff7df103a0d5bffc8
hmarchen <156877222+hmarchen@users.noreply.github.com>
2025-07-31 19:47:07 -0400

    Update YoutubeDL.py

--- yt_dlp/YoutubeDL.py
+++ yt_dlp/YoutubeDL.py
@@ -2656,7 +2656,7 @@ class YoutubeDL:
 
     def _sort_thumbnails(self, thumbnails):
         thumbnails.sort(key=lambda t: (
-            t.get('preference') if t.get('preference') is not None else -1,
+            t.get('preference') if t.get('preference') is not None else 2,
             t.get('width') if t.get('width') is not None else -1,
             t.get('height') if t.get('height') is not None else -1,
             t.get('id') if t.get('id') is not None else '',
